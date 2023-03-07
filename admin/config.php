fcc919fc75c72f8f48c0ff0d65aa5cfdc5673bdd
mehedi1256 <mehedihassanshovospirt@gmail.com>
2023-03-07 13:41:19 +0600

    committed

---
+++ admin/config.php
@@ -0,0 +1,7 @@
+<?php
+$hostname = "http://project-code.epizy.com";
+// $hostname = "http://localhost/LearnPHP/news-site";
+
+$conn = mysqli_connect("localhost","root","","news-site") or die("Connection failed : " . mysqli_connect_error());
+
+?>
